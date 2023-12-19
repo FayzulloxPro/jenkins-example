6e55092c43a6e08bac6b0c1faa9e703baf298701
Fayzullo <qahramonovf26@gmail.com>
2023-12-19 14:36:54 +0500

    commit

---
+++ src/main/java/com/example/model/AuthUser.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String role;
+    private boolean active;
+}
